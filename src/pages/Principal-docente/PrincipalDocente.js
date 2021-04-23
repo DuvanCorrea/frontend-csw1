0bbc36e0e5e8b5b05998de241d73acb3dc026172
DuvanCorrea <duvancorrea96@gmail.com>
2021-04-23 16:57:08 -0500

    style ok

--- src/pages/Principal-docente/PrincipalDocente.js
+++ src/pages/Principal-docente/PrincipalDocente.js
@@ -4,6 +4,8 @@ import { Link, useHistory } from "react-router-dom";
 import IconoSubirMaterial from "../../images/IconoSubirMaterial.svg";
 import Banner from "../../components/banner/Banner";
 import NavbarDocente from '../../components/navbar_docente/NavbarDocente';
+import ImagenSubirMaterial from "../../images/subir material.jpg"
+import ImagenSubirReconocimiento from "../../images/agregar reconocimineto.jpg"
 import "./PrincipalDocente.css";
 
 import docenteContext from "../../context/docenteContext";
@@ -21,18 +23,56 @@ const PrincipalDocente = () => {
   return (
     <>
       <NavbarDocente />
-      <Banner />
+      {/* <Banner /> */}
       <div className="contenedor-docente">
         <div className="contenedor-informacion uno">
-          <img src={IconoSubirMaterial} alt="Subir material" className="icon" />
-          <p className="titulo sub">Subir</p>
-          <p className="titulo">Información</p>
-          <Link to="/Docente/Subir material">
-            <p className="enlaces-p">Material</p>
-          </Link>
-          <Link to="/Docente/Subir reconocimiento">
-            <p className="enlaces-p">Reconocimiento</p>
-          </Link>
+          <div className="opciones_docente">
+            <div className="row">
+
+              {/* SUBRIR MATERIAL */}
+              <div className="col s6">
+                <div className="card">
+                  <div className="card-image waves-effect waves-block waves-light">
+                    <img className="activator" src={ImagenSubirMaterial} />
+                  </div>
+                  <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">Subir materiales<i className="material-icons right">more_vert</i></span>
+                    <p>            <a className="btn" to="/Docente/Subir material">
+                      Subir PDF
+                </a></p>
+                  </div>
+                  <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">Subir materiales<i className="material-icons right">close</i></span>
+                    <p>Como docente puedes subir tus materiales de estudio en formato PDF para que los estudiantes puedan acceder a estos y descargarlos si lo desean.</p>
+                  </div>
+                </div>
+              </div>
+              {/* 433 362 */}
+
+              {/* SUBIR RECONOCIMIENTO */}
+              <div className="col s6">
+
+                <div className="card">
+                  <div className="card-image waves-effect waves-block waves-light">
+                    <img className="activator" src={ImagenSubirReconocimiento} />
+                  </div>
+                  <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">Agregar reconocimiento<i className="material-icons right">more_vert</i></span>
+                    <p><a className="btn" to="/Docente/Subir reconocimiento">
+                      Agregar reconocimiento
+            </a></p>
+                  </div>
+                  <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">Agregar reconocimientos<i className="material-icons right">close</i></span>
+                    <p>Sabemos que eres un exelente docente y por ellos necesita alardear de tus reconocimiento, en esta sección puedes agregalos y seran visibles para todos los que revisen tu perfil.</p>
+                  </div>
+                </div>
+              </div>
+
+
+
+            </div>
+          </div>
         </div>
       </div>
     </>

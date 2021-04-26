5b14aeea696ac98c28a65588dae87de76dd7ddf2
DuvanCorrea <duvancorrea96@gmail.com>
2021-04-26 17:10:32 -0500

    materiales ok

--- src/servicios/config.js
+++ src/servicios/config.js
@@ -1,7 +1,7 @@
 
-export const API_URL = process.env.URL || "http://localHost:4000"
+// export const API_URL = process.env.URL || "http://localHost:4000"
 
 // CONFIGURACION DE DEPLOY
 
-// export const API_URL = process.env.URL || "https://servercsw1.herokuapp.com";
+export const API_URL = process.env.URL || "https://servercsw1.herokuapp.com";
 export const ACTUAL_HOTS = window.location.host
